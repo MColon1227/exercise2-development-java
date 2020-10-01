e62f74d56ae059c646121be3f3185e238cb0b46b
Marisol Colon <mcolon@foundationmedicine.com>
2020-10-01 17:56:57 -0500

    Update code regarding Code Review from Fernando

--- src/exercise2.java
+++ src/exercise2.java
@@ -6,46 +6,136 @@ public class exercise2 {
     public static void main(String[] args) {
         // 1.-Constructor that will initialize a class variable.
         System.out.println("<----- 1.Constructor that will initialize a class variable. ---->");
-        InitializeConstructor example = new InitializeConstructor("Marisol",36);
-        System.out.println(example.toString());
+        InitializeConstructorStudent example = new InitializeConstructorStudent();
+        System.out.println("Name: " + example.getName());
+        System.out.println("Age : " + example.getAge());
 
         // 2.Apply constructor overloading..
         System.out.println("<----- 2.Apply constructor overloading.. ---->");
-        InitializeConstructor example2 = new InitializeConstructor("Gabo",40);
-        System.out.println(example2.toString());
+        InitializeConstructorStudent example2 = new InitializeConstructorStudent("Gabo", 29);
+        System.out.println("Name: " + example2.getName());
+        System.out.println("Age : " + example2.getAge());
+
+        // 2.Apply second constructor overloading..
+        System.out.println("<----- 2.Apply second constructor overloading.. ---->");
+        InitializeConstructorStudent example3 = new InitializeConstructorStudent("Kassandra", 4);
+        System.out.println("Name: " + example3.getName());
+        System.out.println("Age : " + example3.getAge());
 
         // 3.Apply method overloading...
         System.out.println("<----- 3.Apply method overloading... ---->");
         Overloading obj = new Overloading();
         obj.testOverloading("Marisol",36, "Chapalita");
+        obj.testOverloading("CiudadDelSol", "Alexa", 36);
 
+        // 4.Create a property that returns a string....
         System.out.println("<----- 4.Create a property that returns a string.... ---->");
-        Properties person = new Properties();
-        Set names;
-        String newStudent;
+        createProperty person = new createProperty();
+        person.setName("Alexa");
+        person.setAge(28);
+        System.out.println("Person Name: " + person.getName());
+        System.out.println("Person Age: " + person.getAge());
 
-        person.put("Pedro", "New ingress");
-        person.put("Karlita", "New ingress");
-        names = person.keySet();
-
-        for (Object name : names) {
-            newStudent = (String) name;
-            System.out.println("The name " + newStudent + " is " + person.getProperty(newStudent) + ".");
-        }
-        newStudent = person.getProperty("Alonso", "Not Found");
-        System.out.println("The name Alonso is " + newStudent + ".");
-
+        // 5.Create a method to separate a list of 20 numbers between odds and even.....
         System.out.println("<-----5.Create a method to separate a list of 20 numbers between odds and even..... ---->");
         System.out.printf("", separateNumbers());
 
         System.out.println("<-----6.Create a method to print numbers in the following pattern - from 0 to 20...... ---->");
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number 4 to indicated the rows");
+        printingPattern();
+
+
+    }
+
+    // 1.-Constructor that will initialize a class variable.
+    // 2.Apply constructor overloading..
+    static class InitializeConstructorStudent {
+        static String name;
+        static int age;
+        InitializeConstructorStudent() {
+            name = "Marisol";
+            age = 36;
+        }
+        InitializeConstructorStudent(String name1, int age1) {
+            name = name1;
+            age = age1;
+        }
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public int getAge() {
+            return age;
+        }
+        public void setAge(int age) {
+            this.age = age;
+        }
+    }
+
+    // 3.Apply method overloading...
+    static class Overloading {
+        public void testOverloading(String name, int age, String address)
+        {
+            System.out.println("Method1 testOverloading: " + name + " : " + age + " : " + address );
+        }
+        public void testOverloading(String address, String name, int age)
+        {
+            System.out.println("Method2 testOverloading: " + address + " : " + name + " : " + age );
+        }
+    }
+
+    // 4.Create a property that returns a string....
+    public static class createProperty {
+        private String name;
+        private int age;
+
+        public String getName(){
+            return name;
+        }
+        public int getAge(){
+            return age;
+        }
+        public void setName(String newName){
+            name = newName;
+        }
+        public void setAge(int newAge){
+            age = newAge;
+        }
+    }
+
+    // 5.Create a method to separate a list of 20 numbers between odds and even.....
+    public static Object separateNumbers() {
+        int numberList1[] = {1,2,5,6,3,4,12,15,18,28,33,89,7,0,9,11,23,22,16,20};
+        int numberList2[] = {41,57,5,5,78,1,2,9,54,7,6,20,14};
+        System.out.println("Odd Numbers List1:");
+        for(int i=0; i<numberList1.length; i++){
+            if (numberList1[i]%2!=0){
+                System.out.println(numberList1[i]);
+            }
+        }
+        System.out.println("Even Numbers List1:");
+        for(int i=0;i<numberList1.length;i++){
+            if(numberList1[i]%2==0){
+                System.out.println(numberList1[i]);
+            }
+        }
+        System.out.println("Even Numbers List2:");
+        for(int i=0;i<numberList2.length;i++){
+            if(numberList2[i]%2==0){
+                System.out.println(numberList2[i]);
+            }
+        }
+        return null;
+    }
+
+    // 6.Create a method to print numbers in the following pattern - from 0 to 20......
+    public static void printingPattern() {
+        Scanner scanner = new Scanner(System.in);
         int rows = scanner.nextInt();
         int test = 1;
-        System.out.println("Printing Pattern");
-        for (int i = 1; i <= rows; i++)
-        {
+        for (int i = 1; i <= rows; i++) {
             for (int j = 1; j <= i; j++)
             {
                 System.out.print(test + " ");
@@ -63,50 +153,5 @@ public class exercise2 {
             System.out.println();
         }
     }
-
-    // 1.-Constructor that will initialize a class variable.
-    // 2.Apply constructor overloading..
-    public static class InitializeConstructor
-    {
-        public String toString()
-        {
-            return name + "," + number;
-        }
-        private String name;
-        private int number;
-        public InitializeConstructor(String name, int number)
-        {
-            this.name = name;
-            this.number = number;
-        }
-    }
-
-    // 3.Apply method overloading...
-    static class Overloading
-    {
-        public void testOverloading(String name, int number, String address)
-        {
-            System.out.println("Method testOverloading");
-        }
-    }
-
-    public static Object separateNumbers()
-    {
-        int numberList[] = {1,2,5,6,3,4,12,15,18,28,33,89,7,0,9,11,23,22,16,20};
-        System.out.println("Odd Numbers:");
-        for(int i=0; i<numberList.length; i++){
-            if (numberList[i]%2!=0){
-                System.out.println(numberList[i]);
-            }
-        }
-        System.out.println("Even Numbers:");
-        for(int i=0;i<numberList.length;i++){
-            if(numberList[i]%2==0){
-                System.out.println(numberList[i]);
-            }
-        }
-        return null;
-    }
-
-
 }
+
